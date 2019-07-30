bdf27af70d127d57798d54608e922d37f0c6fee1
Gennerator <VladutzBoy2010@Gmail.Com>
2019-07-31 01:09:54 +0300

    Replace Grunt with Gulp and revise some files

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+const gulp = require('gulp');
+const path = require('path');
+
+const closureCompiler = require('google-closure-compiler').gulp();
+const crass = require('gulp-crass');
+const htmlmin = require('gulp-htmlmin');
+const brotli = require('gulp-brotli');
+
+function jsMin(f) {
+  return gulp.src(f, {base: './'})
+      .pipe(closureCompiler({
+        compilation_level: 'ADVANCED',
+        language_in: 'ECMASCRIPT_2019',
+        assume_function_wrapper: true,
+        js_output_file: f.substring(13),
+        output_wrapper: '(function(){%output%})();',
+        jscomp_off: 'checkVars',
+        warning_level: 'VERBOSE',
+      }, {
+        platform: ['native', 'java', 'javascript'],
+      }))
+      .pipe(gulp.dest(`./static/js/`))
+      .on('end', function() {
+        console.log(` [brotlify] ${f.replace('staticDev/', 'static/')}.br`);
+        brotlify(f.replace('staticDev/', 'static/'));
+      });
+}
+
+function cssMin(f) {
+  return gulp.src(f)
+      .pipe(crass({
+        optimize: true,
+        O1: true,
+      }))
+      .pipe(gulp.dest(`./static/css/`))
+      .on('end', function() {
+        console.log(` [brotlify] ${f.replace('staticDev/', 'static/')}.br`);
+        brotlify(f.replace('staticDev/', 'static/'));
+      });
+}
+
+function htmlMin(f) {
+  return gulp.src(f)
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeEmptyElements: false,
+        removeOptionalTags: false,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        sortAttributes: true,
+        sortClassName: true,
+      }))
+      .pipe(gulp.dest(path.dirname(f.replace('viewsDev/', 'views/'))));
+}
+
+function brotlify(f) {
+  return gulp.src(f)
+      .pipe(brotli.compress({
+        skipLarger: true,
+        mode: 0,
+        quality: 11,
+        lgwin: 24,
+        lgblock: 0,
+      }))
+      .pipe(gulp.dest(path.dirname(f)));
+}
+
+function watcher() {
+  gulp.watch('./staticDev/js/*.js').on('change', function(f) {
+    jsMin(f);
+    console.log(` [jsMin] ${f}`);
+  });
+
+  gulp.watch('./staticDev/css/*.css').on('change', function(f) {
+    cssMin(f);
+    console.log(` [cssMin] ${f}`);
+  });
+  gulp.watch('./viewsDev/**/*.handlebars').on('change', function(f) {
+    htmlMin(f);
+    console.log(` [htmlMin] ${f}`);
+  });
+}
+
+exports.watcher = watcher;
--- staticDev/js/script.js
+++ staticDev/js/script.js
@@ -3,8 +3,10 @@ link.rel='stylesheet',
 link.href='/css/style.css',
 document.head.appendChild(link);
 
-const b = window.location.pathname.substring(1);
-if (document.getElementById(b)) document.getElementById(b).className+=' selected';
+window.addEventListener('DOMContentLoaded', function() {
+  const b = window.location.pathname.substring(1);
+  if (document.getElementById(b)) document.getElementById(b).className+=' selected';
+}, false);
 
 function setAttributes(elem, attr) {
   for (const n in attr) {
