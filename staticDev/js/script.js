dbc362ff359bd117fa96248067686402160810a0
Gennerator <VladutzBoy2010@Gmail.Com>
2019-08-05 21:52:34 +0300

    Add XHR and SnackBar helper functions

--- staticDev/js/script.js
+++ staticDev/js/script.js
@@ -24,4 +24,37 @@ function setAttributes(elem, attr) {
   }
 }
 
+function xhr(method, url, req, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify(req));
+  xhr.onload = function() {
+    cb(xhr.response);
+  };
+}
+
+function snackbar(msg, type = 0) {
+  const snackB = document.createElement('div');
+  snackB.setAttribute('id', 'snackbar');
+  const snackC = document.createElement('div');
+  snackC.setAttribute('id', 'snackC');
+  snackC.textContent = msg;
+  switch (type) {
+    case 0: snackC.className = 'green'; break;
+    case 1: snackC.className = 'orange'; break;
+    case 2: snackC.className = 'red'; break;
+    case 3: snackC.className = 'blue'; break;
+  }
+  snackB.appendChild(snackC);
+  document.body.appendChild(snackB);
+  snackB.className = 'show';
+  setTimeout(function() {
+    snackB.className = '';
+    snackB.parentNode.removeChild(snackB);
+  }, 3000);
+}
+
 window['setAttributes'] = setAttributes;
+window['xhr'] = xhr;
+window['snackbar'] = snackbar;
