fa7a5afc81ea045a93e63648825b12e4cabe04cb
Gennerator <VladutzBoy2010@Gmail.Com>
2019-08-06 23:02:54 +0300

    Move add header job from Nginx to Node and fix static error pages

--- app.js
+++ app.js
@@ -45,10 +45,18 @@ app.use(session({
   },
 }));
 
-app.use(function(req, res, next) {
+app.get('*', function(req, res, next) {
   res.locals.nonce = crypto.randomBytes(16).toString('base64');
   res.header('Content-Security-Policy', `default-src 'none'; script-src 'self' 'strict-dynamic' 'nonce-${res.locals.nonce}'; img-src 'self'; connect-src 'self'; style-src 'self' 'nonce-${res.locals.nonce}'; font-src 'self'; object-src 'none'; media-src 'self'; form-action 'self'; frame-ancestors 'none'; base-uri 'none'; manifest-src 'self'; report-uri https://gennerator.report-uri.com/r/d/csp/enforce; report-to default`);
+  res.header('Feature-Policy', `accelerometer 'none'; ambient-light-sensor 'none'; autoplay 'none'; camera 'none'; encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; speaker 'self'; sync-xhr 'none'; usb 'none'; vr 'none'`);
+  res.header('Expect-CT', `max-age=1209600, enforce, report-uri=https://gennerator.report-uri.com/r/d/ct/enforce`);
+  res.header('Report-To', `{"group":"default","max_age":31536000,"endpoints":[{"url":"https://gennerator.report-uri.com/a/d/g"}],"include_subdomains":true}`);
+  res.header('Strict-Transport-Security', `max-age=31536000; includeSubDomains; preload`);
+  res.header('Referrer-Policy', `strict-origin-when-cross-origin`);
+  next();
+});
 
+app.use(function(req, res, next) {
   res.locals.userName = req.session.username;
   res.locals.greetingMessage = util.greetingMessage();
 
@@ -72,7 +80,11 @@ app.get('/partajare', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-  res.render('login');
+  if (!res.locals.userName) {
+    res.render('login');
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/logOut', function(req, res) {
@@ -129,11 +141,6 @@ app.post('/usernameExists', function(req, res) {
 
 app.use(function(req, res) {
   res.status(404).render('404');
-});
-
-app.use(function(err, req, res, next) {
-  console.log(err);
-  res.status(500).render('500');
 });
 
 app.listen(port, () => console.log(`Aplicatia ruleaza pe portul ${port}!`));
