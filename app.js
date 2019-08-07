30426b1f27a056339b40ba03b946dfc13f652616
Gennerator <VladutzBoy2010@Gmail.Com>
2019-08-07 23:20:48 +0300

    Make CSP nonce generation async and generate session id only on login

--- app.js
+++ app.js
@@ -39,19 +39,23 @@ app.use(session({
     ttl: 43200, // 12 hours
   }),
   genid: function(req) {
-    return crypto.randomBytes(128).toString('base64');
+    if (req.originalUrl == '/loginUser') {
+      return crypto.randomBytes(128).toString('base64');
+    }
   },
 }));
 
 app.get('*', function(req, res, next) {
-  res.locals.nonce = crypto.randomBytes(16).toString('base64');
-  res.header('Content-Security-Policy', `default-src 'none'; script-src 'self' 'strict-dynamic' 'nonce-${res.locals.nonce}'; img-src 'self'; connect-src 'self'; style-src 'self' 'nonce-${res.locals.nonce}'; font-src 'self'; object-src 'none'; media-src 'self'; form-action 'self'; frame-ancestors 'none'; base-uri 'none'; manifest-src 'self'; report-uri https://gennerator.report-uri.com/r/d/csp/enforce; report-to default`);
-  res.header('Feature-Policy', `accelerometer 'none'; ambient-light-sensor 'none'; autoplay 'none'; camera 'none'; encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; speaker 'self'; sync-xhr 'none'; usb 'none'; vr 'none'`);
-  res.header('Expect-CT', `max-age=1209600, enforce, report-uri=https://gennerator.report-uri.com/r/d/ct/enforce`);
-  res.header('Report-To', `{"group":"default","max_age":31536000,"endpoints":[{"url":"https://gennerator.report-uri.com/a/d/g"}],"include_subdomains":true}`);
-  res.header('Strict-Transport-Security', `max-age=31536000; includeSubDomains; preload`);
-  res.header('Referrer-Policy', `strict-origin-when-cross-origin`);
-  next();
+  crypto.randomBytes(16, (e, b) => {
+    res.locals.nonce = b.toString('base64');
+    res.header('Content-Security-Policy', `default-src 'none'; script-src 'self' 'strict-dynamic' 'nonce-${res.locals.nonce}'; img-src 'self'; connect-src 'self'; style-src 'self' 'nonce-${res.locals.nonce}'; font-src 'self'; object-src 'none'; media-src 'self'; form-action 'self'; frame-ancestors 'none'; base-uri 'none'; manifest-src 'self'; report-uri https://gennerator.report-uri.com/r/d/csp/enforce; report-to default`);
+    res.header('Feature-Policy', `accelerometer 'none'; ambient-light-sensor 'none'; autoplay 'none'; camera 'none'; encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; speaker 'self'; sync-xhr 'none'; usb 'none'; vr 'none'`);
+    res.header('Expect-CT', `max-age=1209600, enforce, report-uri=https://gennerator.report-uri.com/r/d/ct/enforce`);
+    res.header('Report-To', `{"group":"default","max_age":31536000,"endpoints":[{"url":"https://gennerator.report-uri.com/a/d/g"}],"include_subdomains":true}`);
+    res.header('Strict-Transport-Security', `max-age=31536000; includeSubDomains; preload`);
+    res.header('Referrer-Policy', `strict-origin-when-cross-origin`);
+    next();
+  });
 });
 
 app.use(function(req, res, next) {
