1cc476211dc8d227b52e4280a1f52badce40a59e
Gennerator <VladutzBoy2010@Gmail.Com>
2019-08-22 19:34:27 +0300

    Add example file for env.js and update database structure

---
+++ example/env.example.js
@@ -0,0 +1,2 @@
+process.env.PORT = 1234; 
+process.env.COOKIE_SECRET = 'my cookie secret'; // change this to a random string
--- example/website.sql
+++ example/website.sql
@@ -1,14 +1,14 @@
 /*
+
  Source Server Type    : PostgreSQL
- Source Server Version : 110004
+ Source Server Version : 110005
  Source Catalog        : gennerator
  Source Schema         : website
 
  Target Server Type    : PostgreSQL
- Target Server Version : 110004
+ Target Server Version : 110005
  File Encoding         : 65001
-
- Date: 07/08/2019 21:10:11
+ 
 */
 
 
@@ -19,7 +19,7 @@ DROP SEQUENCE IF EXISTS "website"."user_id_seq";
 CREATE SEQUENCE "website"."user_id_seq" 
 INCREMENT 1
 MINVALUE  1
-MAXVALUE 32767
+MAXVALUE 36000
 START 1
 CACHE 1;
 
@@ -42,12 +42,20 @@ CREATE TABLE "website"."users" (
   "user_id" int2 NOT NULL DEFAULT nextval('"website".user_id_seq'::regclass),
   "username" varchar(40) COLLATE "pg_catalog"."default" NOT NULL,
   "password" char(246) COLLATE "pg_catalog"."default" NOT NULL,
+  "email" varchar(254) COLLATE "pg_catalog"."default" NOT NULL,
   "usertype" int2 NOT NULL DEFAULT 1,
   "active" bool NOT NULL DEFAULT false,
   "createdate" timestamptz(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "lastlogin" timestamptz(6) NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 ;
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "website"."user_id_seq"
+OWNED BY "website"."users"."user_id";
+SELECT setval('"website"."user_id_seq"', 1, true);
 
 -- ----------------------------
 -- Primary Key structure for table session
@@ -58,6 +66,7 @@ ALTER TABLE "website"."session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid"
 -- Uniques structure for table users
 -- ----------------------------
 ALTER TABLE "website"."users" ADD CONSTRAINT "users_username_key" UNIQUE ("username");
+ALTER TABLE "website"."users" ADD CONSTRAINT "users_email_key" UNIQUE ("email");
 
 -- ----------------------------
 -- Primary Key structure for table users
