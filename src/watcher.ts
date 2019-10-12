169f1796577b14dbe30291d3618d69bec6bf2168
Gennerator <VladutzBoy2010@Gmail.Com>
2019-10-13 02:06:51 +0300

    Remake the file watcher JS minification and file compression
    -Use the java version of the Closure Compiler
    -Do all 'TS to JS to minified JS' in memory for improved performance
    -Keep the 'use strict'; statement in all minified JS
    -Replace the 'iltorb' library with the native 'zlib' one for file
    compression and add gzip as a fallback from brotli
    -Add more documentation to ambigous code and fix overall readability

--- src/watcher.ts
+++ src/watcher.ts
@@ -2,17 +2,22 @@
 
 import chokidar = require('chokidar');
 import { promises as fs } from 'fs';
+import { spawn } from 'child_process';
+import { brotliCompress, gzip, constants } from 'zlib';
 
 import typescript = require('typescript');
 import crass = require('crass');
-import { compiler } from 'google-closure-compiler';
 import { minify } from 'html-minifier';
-import { compress } from 'iltorb';
 
+/**
+ * The path to all front-end JS
+ */
 const pathSrcJs = 'src/static/js/';
 
+const compressibleFileTypes = /.*\.(css|js|html|svg|xml|webmanifest|json)$/;
+
 const watcher = chokidar.watch(['./src', './app'], {
-  ignored: /.*\.br$/,
+  ignored: /.*\.(br|gz)$/,
   persistent: true,
   depth: 3,
   awaitWriteFinish: {
@@ -45,28 +50,32 @@ watcher
           break;
         default:
           fs.unlink(path.replace('src/', 'app/')).catch((error) => log(error));
-          if (/.*\.(css|js|html|svg|xml|webmanifest|json)/.test(extension)) {
+          if (compressibleFileTypes.test(extension)) {
             log(`[CLEANUP] ${path}`);
             fs.unlink(`${path.replace('src/', 'app/')}.br`).catch((error) => log(error));
+            fs.unlink(`${path.replace('src/', 'app/')}.gz`).catch((error) => log(error));
           }
       }
     }
   });
 
 /**
- * Function used on ADD/CHANGE events in chokidar watcher instance
+ * Function used on ADD/CHANGE events in the Chokidar Watcher instance
  * @param path Path of the file
  */
 function onchange(path: string): void {
   const extension = path.substring(path.indexOf('.'));
   if (path.startsWith('src/')) {
-    const writepath = path.replace('src/', 'app/');
+    const writePath = path.replace('src/', 'app/');
     switch (extension) {
       case '.ts':
         log(`[TS->JS] ${path}`);
         if (path.startsWith(pathSrcJs)) {
           fs.readdir(pathSrcJs)
             .then((files) => {
+              /**
+               * Read all files from 'src/static/js'.
+               */
               const readBuffer: Promise<Buffer>[] = [];
               for (let i = 0; i < files.length; i++) {
                 const filepath = pathSrcJs + files[i];
@@ -75,15 +84,22 @@ function onchange(path: string): void {
               }
               Promise.all(readBuffer)
                 .then((buffers) => {
-                  const writeAll: Promise<void>[] = [];
+                  let concat = '';
                   for (let i = 0; i < buffers.length; i++) {
-                    writeAll.push(fs.writeFile(`example/${files[i].slice(0, -2)}js`, tsJS(buffers[i].toString())));
+                    /**
+                     * Add 'window.fileName' variable with the file name as its value at the end of each file (to be
+                     * able to differentiate them later) and concatenate them all.
+                     */
+                    concat += tsJS(buffers[i].toString() + `window.fileName='${files[i].slice(0, -2)}js';`);
                   }
-                  Promise.all(writeAll)
-                    .then(() => {
-                      jsMin();
-                    })
-                    .catch((error) => log(error));
+                  /**
+                   * Google Closure Compiler does not like multiple 'use strict'; statements so remove them all
+                   * He also does not like the '__esModule' property that gets added by the TypeScript transpiler
+                   * so remove it too if exists.
+                   */
+                  jsMin(concat.
+                    replace(/'use strict';/g, '').
+                    replace(/Object\.defineProperty\(exports, "__esModule", { value: true }\);/g, ''));
                 })
                 .catch((error) => log(error));
             })
@@ -91,7 +107,7 @@ function onchange(path: string): void {
         } else {
           fs.readFile(path)
             .then((buffer) => {
-              fs.writeFile(`${writepath.slice(0, -2)}js`, tsJS(buffer.toString())).catch((error) => log(error));
+              fs.writeFile(`${writePath.slice(0, -2)}js`, tsJS(buffer.toString())).catch((error) => log(error));
             })
             .catch((error) => log(error));
         }
@@ -100,7 +116,7 @@ function onchange(path: string): void {
         log(`[CSSmin] ${path}`);
         cssMin(path)
           .then((min) => {
-            fs.writeFile(writepath, min).catch((error) => log(error));
+            fs.writeFile(writePath, min).catch((error) => log(error));
           })
           .catch((error) => log(error));
         break;
@@ -108,7 +124,7 @@ function onchange(path: string): void {
         log(`[JSONmin] ${path}`);
         fs.readFile(path)
           .then((buffer) => {
-            fs.writeFile(writepath, JSON.stringify(JSON.parse(buffer.toString()))).catch((error) => log(error));
+            fs.writeFile(writePath, JSON.stringify(JSON.parse(buffer.toString()))).catch((error) => log(error));
           })
           .catch((error) => log(error));
         break;
@@ -118,101 +134,86 @@ function onchange(path: string): void {
         log(`[HTMLmin] ${path}`);
         htmlMin(path)
           .then((min) => {
-            fs.writeFile(writepath, min).catch((error) => log(error));
+            fs.writeFile(writePath, min).catch((error) => log(error));
           })
           .catch((error) => log(error));
         break;
       default:
         log(`[COPY] ${path}`);
-        fs.copyFile(path, writepath).catch((error) => log(error));
+        fs.copyFile(path, writePath).catch((error) => log(error));
     }
   } else {
     if (path.startsWith('app/static/')) {
-      if (/.*\.(css|js|html|svg|xml|webmanifest|json)$/.test(extension)) {
-        log(`[Brotli] ${path}`);
-        brotlify(path);
+      if (compressibleFileTypes.test(extension)) {
+        log(`[Compress] ${path}`);
+        compress(path);
       }
     }
   }
 }
 
 /**
- * Compresses files using Brotli
- * @param f Relative path of the file
+ * Minifies JS code with Google Closure Compiler
+ * @param code JS code
  */
-function brotlify(f: string): void {
-  fs.readFile(f)
-    .then((buffer) => {
-      compress(buffer, {
-        'mode': 1, // text
-        'quality': 11,
-        'lgwin': 24,
-        'lgblock': 0,
-      })
-        .then((compressed: Buffer) => {
-          if (compressed.byteLength < buffer.byteLength) {
-            fs.writeFile(`${f}.br`, compressed).catch((error) => log(error));
-          } else {
-            log('Compressed > Original file');
-          }
-        })
-        .catch((error) => log(error));
-    })
-    .catch((error) => log(error));
-}
+function jsMin(code: string): void {
+  const child = spawn('java', ['-jar', 'node_modules/google-closure-compiler-java/compiler.jar',
+    '--compilation_level=ADVANCED',
+    '--language_in=ECMASCRIPT_2019',
+    '--language_out=ECMASCRIPT_2017',
+    '--assume_function_wrapper',
+    '--use_types_for_optimization',
+    '--strict_mode_input',
+    '--charset=UTF-8',
+    '--externs=externs/YoutubeAPI.js'
+  ]);
+  child.stdin.setDefaultEncoding('utf8');
+  child.stdin.write(code);
+  child.stdin.end();
 
-/**
- * Minifies JS files with Closure Compiler
- */
-function jsMin(): void {
-  new compiler({
-    'js': 'example/*.js',
-    'compilation_level': 'ADVANCED',
-    'language_in': 'ECMASCRIPT_2019',
-    'language_out': 'ECMASCRIPT_2017',
-    'assume_function_wrapper': true,
-    'use_types_for_optimization': true,
-    'charset': 'UTF-8',
-    'externs': 'externs/YoutubeAPI.js',
-  }).run((exitCode: number, stdOut: string, stdErr: string) => {
-    if (exitCode == 0) {
-      if (stdErr) {
-        log(`=============================================\n${stdErr}=============================================\n`);
-      }
-      const f = stdOut.split(/\/\*\n\s(.*).\*\/\n/);
-      if (f.length > 0) {
-        const writeAll: Promise<void>[] = [];
-        for (let i = 1; i < f.length; i += 2) {
-          writeAll.push(fs.writeFile(`app/static/js/${f[i]}`, f[i + 1].replace(/\n/g, '')));
-        }
-        Promise.all(writeAll)
-          .then(() => {
-            fs.readdir('example/')
-              .then((files) => {
-                for (let i = 0; i < files.length; i++) {
-                  if (files[i].endsWith('.js')) {
-                    fs.unlink(`example/${files[i]}`).catch((error) => log(error));
-                  }
-                }
-              })
-              .catch((error) => log(error));
-          })
-          .catch((error) => log(error));
-      }
+  child.stderr.on('data', function(data: Buffer) {
+    console.log(data.toString());
+  });
+
+  let output: Buffer;
+  child.stdout.on('data', (data: Buffer) => {
+    if (!output) {
+      output = data;
     } else {
-      log(`Code: [${exitCode}]`);
-      log(`Error: [${stdErr}]`);
+      output = Buffer.concat([output, data]);
+    }
+  });
+
+  child.stdout.on('end', function() {
+    /**
+     * Remove all endlines left behind by the Closure Compiler and split the code based on the 'window.fileName'
+     * variable defined earlier.
+     *
+     * The 'splitBuffer' is structured like this:
+     *  - odd indexes are the file names
+     *  - even indexes are the 'compiled' code
+     *  - last item is an empty string
+     */
+    const splitBuffer = output.toString().replace(/\n/g, '').split(/window\.fileName="(.*?)";/);
+    if (splitBuffer) {
+      /**
+       * First file always starts with the 'use strict'; statement so it needs to be handled separately.
+       */
+      fs.writeFile(`app/static/js/${splitBuffer[1]}`, splitBuffer[0]);
+      for (let i = 2; i < splitBuffer.length - 1; i += 2) {
+        fs.writeFile(`app/static/js/${splitBuffer[i + 1]}`, '\'use strict\';' + splitBuffer[i]);
+      }
     }
   });
 }
 
 /**
  * Transpiles TypeScript to JavaScript code
- * @param f TypeScript code
+ * @param code TypeScript code
  * @return JavaScript code
  */
-function tsJS(f: string): string {
-  return typescript.transpileModule(f, {
+function tsJS(code: string): string {
+  return typescript.transpileModule(code, {
     'compilerOptions': {
       'module': typescript.ModuleKind.CommonJS,
       'target': typescript.ScriptTarget.ES2020,
@@ -227,12 +228,55 @@ function tsJS(f: string): string {
 }
 
 /**
+ * Compresses files using Brotli and Gzip
+ * @param path Relative path of the file
+ */
+function compress(path: string): void {
+  fs.readFile(path)
+    .then((buffer) => {
+      brotliCompress(buffer, {
+        params: {
+          [constants.BROTLI_PARAM_MODE]: 1,     // text
+          [constants.BROTLI_PARAM_QUALITY]: 11, // max quality
+          [constants.BROTLI_PARAM_LGWIN]: 24,   // max sliding window size (LZ77)
+          [constants.BROTLI_PARAM_LGBLOCK]: 0,  // auto input block size
+        },
+      }, (error, compressedBuffer) => {
+        if (!error) {
+          if (compressedBuffer.byteLength < buffer.byteLength) {
+            fs.writeFile(`${path}.br`, compressedBuffer).catch((error) => log(error));
+          } else {
+            log('[Brotli] Compressed > Original file');
+          }
+        } else {
+          log(error);
+        }
+      });
+      gzip(buffer, {
+        level: 9,    // max quality
+        memLevel: 9, // max memory usage
+      }, (error, compressedBuffer) => {
+        if (!error) {
+          if (compressedBuffer.byteLength < buffer.byteLength) {
+            fs.writeFile(`${path}.gz`, compressedBuffer).catch((error) => log(error));
+          } else {
+            log('[GZip] Compressed > Original file');
+          }
+        } else {
+          log(error);
+        }
+      });
+    })
+    .catch((error) => log(error));
+}
+
+/**
  * Minifies CSS with Crass
- * @param f Relative path of the css file
+ * @param path Relative path of the CSS file
  * @return Minified CSS code
  */
-async function cssMin(f: string): Promise<string> {
-  const file = await fs.readFile(f);
+async function cssMin(path: string): Promise<string> {
+  const file = await fs.readFile(path);
   return crass
     .parse(file.toString())
     .optimize({ o1: true })
@@ -241,11 +285,11 @@ async function cssMin(f: string): Promise<string> {
 
 /**
  * Minifies files with HTML-minifier
- * @param f Relative path of the file
+ * @param path Relative path of the file
  * @return Minified code
  */
-async function htmlMin(f: string): Promise<string> {
-  const file = await fs.readFile(f);
+async function htmlMin(path: string): Promise<string> {
+  const file = await fs.readFile(path);
   return minify(file.toString(), {
     collapseWhitespace: true,
     collapseBooleanAttributes: true,
